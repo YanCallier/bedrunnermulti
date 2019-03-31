6c2fd7c87b1318831b9e3737400c78ec82d9fb78
Callier <45624004+YanCallier@users.noreply.github.com>
2019-03-31 17:23:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ io.on('connection', function (socket) {
         for (var runner in connections) {
             if (connections[runner].runnerState === 'running') nbRunner += 1;
         }
-
+        console.log (">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + nbRunner);
         if ( nbRunner === 1) io.emit('lightUp');
         if ( nbRunner === 0) partieEnCours = false;
 
